030a793afa04f6bc3649fd14edfe41d801c70b75
Jean-Philippe Roemer <jp@roemer.im>
2018-06-05 14:20:24 +0100

    xd60/keymaps/0rax: add mapping ascii art

--- keyboards/xd60/keymaps/0rax/keymap.c
+++ keyboards/xd60/keymaps/0rax/keymap.c
@@ -2,13 +2,26 @@
 #include "action_layer.h"
 
 // Layer definitions
-#define _BL 0
-#define _FL 1
+enum x60_layers {
+  _BL,
+  _FL,
+};
 
 // Layout Layers
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // 0: Base Layer
+  // ,-----------------------------------------------------------------------------------------.
+  // | ESC`|  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  +  | BACKSPACE |
+  // |-----------------------------------------------------------------------------------------|
+  // | TAB    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |  Enter |
+  // |---------------------------------------------------------------------------------|       |
+  // | BSPC    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |  ~  |       |
+  // |-----------------------------------------------------------------------------------------|
+  // | SFT |  \  |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  <  |  >  |  ?  | SFT | UP↑ | DEL |
+  // |-----------------------------------------------------------------------------------------|
+  // | LSFT | LGUI | LALT | LGUI |             SPACE             | ALT | FN  | LF← | DW↓ | RG→ |
+  // `-----------------------------------------------------------------------------------------'
   [_BL] = LAYOUT_all(
       KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,   \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_NO,   \
@@ -17,16 +30,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                          KC_RGUI, F(0),     KC_LEFT, KC_DOWN,  KC_RIGHT),
 
   // 1: Function Layer
+  // ,-----------------------------------------------------------------------------------------.
+  // |RESET| F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |    DELETE |
+  // |-----------------------------------------------------------------------------------------|
+  // |        |     |     |     |     |     |     |     |     |     |     |     |     |  Enter |
+  // |---------------------------------------------------------------------------------|       |
+  // | DELETE  |     |     |VOLM↓|VMUTE|VOLM↑|MED◂◂|MED▮▮|MED▸▸|     |     |     |     |       |
+  // |-----------------------------------------------------------------------------------------|
+  // | SFT |RGBTG|RGBMD| CUT |COPY |PASTE|RGBH↑|RGBH↓|RGBS↑|RGBS↓|RGBB↑|RGBB↓|HOME | PG↑ | END |
+  // |-----------------------------------------------------------------------------------------|
+  // | LSFT | LGUI | LALT | LGUI |             SPACE             | ALT | FN  |     | PG↓ |     |
+  // `-----------------------------------------------------------------------------------------'
   [_FL] = LAYOUT_all(
       RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,  KC_NO,  \
       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,            KC_NO,  \
       KC_DEL,  KC_NO,   KC_NO,   KC_VOLD,KC_MUTE,KC_VOLU, KC_MPRV,KC_MPLY,KC_MNXT,KC_NO,  KC_NO,   KC_NO,   KC_NO,              KC_ENT, \
       KC_LSFT, RGB_TOG, RGB_MOD, KC_CUT,  KC_COPY,KC_PASTE,RGB_HUI,RGB_HUD,RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD, KC_HOME, KC_PGUP,   KC_END, \
       KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                          KC_RGUI, F(0),   KC_NO,   KC_PGDOWN, KC_NO),
-
 };
 
 // Custom actions definitions (defines action to run on call to F(idx))
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(_FL),  // F(0) will swich to _FL_ layer when pressed
+    [0] = ACTION_LAYER_MOMENTARY(_FL),  // F(0) will swich to _FL layer when pressed
 };
