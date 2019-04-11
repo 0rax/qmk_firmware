e109c40fe1641e491abf86600bb6a8f4b4dfebdc
Jean-Philippe Roemer <jp@roemer.im>
2019-04-11 23:48:45 +0100

    idobo/keymaps/0rax_split: move split mapping to its own folder

--- keyboards/idobo/keymaps/0rax/config.h
+++
@@ -1,4 +0,0 @@
-#ifndef ID75_0RAX_H
-#define ID75_0RAX_H
-
-#endif
--- keyboards/idobo/keymaps/0rax_split/rules.mk
+++ keyboards/idobo/keymaps/0rax_split/rules.mk
@@ -1,3 +1,6 @@
+# Override Userspace
+USER_NAME := 0rax
+
 # Build Options
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
@@ -7,7 +10,7 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
-SLEEP_LED_ENABLE = yes       # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
 
 # Add custon C files
 SRC += hooks.c
